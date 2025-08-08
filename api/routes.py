30410c6b754844507ff218e406c5609e849827be
sb02dev <sb02dev@gmail.coim>
2025-08-08 15:51:11 +0200

    sample route is loaded from file instead of generating it in code (also tracks are included)

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,7 @@
 import asyncio
 import datetime
 from http import HTTPStatus
+from pathlib import Path
 import time
 import json
 import io
@@ -516,91 +517,12 @@ async def websocket_endpoint(websocket: WebSocket, session_id: str = None):
 
 
 def default_route():
-    import math
-    rgen = VFRFunctionRoute(
-        "LHFH--Lovasberény--Császár--Nyergesújfalu--LHFH",
-        100,
-        # datetime.datetime(2025, 6, 9, 7, 0, tzinfo=datetime.timezone.utc),
-        datetime.datetime.now(datetime.timezone.utc) +
-        datetime.timedelta(days=2),
-        session=global_requests_session,
-        workfolder=os.path.join(rootpath, "data"),
-        outfolder=os.path.join(rootpath, "output"),
-        tracksfolder=os.path.join(rootpath, "data")
-    )
-
-    tl = VFRPoint(18.05268767493478, 47.770130324899284,
-                  VFRCoordSystem.LONLAT, rgen).project_point(VFRCoordSystem.MAP_XY)
-    br = VFRPoint(19.021069644306213, 47.28705538442129, 
-                  VFRCoordSystem.LONLAT, rgen).project_point(VFRCoordSystem.MAP_XY)
-    rgen.set_area_of_interest(tl.x, tl.y, br.x, br.y)
-
-    rgen.set_state(VFRRouteState.AREAOFINTEREST)
-
-    rgen.add_waypoint("LHFH", VFRPoint(18.912424867046774, 47.48950030173632, VFRCoordSystem.LONLAT, rgen))
-    rgen.add_waypoint("Lovasberény", VFRPoint(18.55314689455907, 47.31145066437747, VFRCoordSystem.LONLAT, rgen))
-    rgen.add_waypoint("Császár", VFRPoint(18.13993451767051, 47.50100085714328, VFRCoordSystem.LONLAT, rgen))
-
-    rgen.set_state(VFRRouteState.LEGS) # because we also want to add annotations
-
-    rgen.legs = [] # TODO: currently recreate (should be updating and calculating lambdas)
-    rgen.add_leg(
-        'LHFH->Lovasberény',
-        '\\sqrt[3]{{x}}',
-        '$x=0$ at Lovasberény, $x=1.5$ at LHFH',
-        lambda x: x ** (1. / 3.),
-        [
-            # (lat, lon, x),
-            (VFRPoint(18.912424867046774, 47.48950030173632), 1.5),
-            (VFRPoint(18.55314689455907, 47.31145066437747), 0)
-        ]
-    ).add_annotation("START", 1.5, (-120, 10)) \
-    .add_annotation("Etyek", 0.8, (20, -25)) \
-    .add_annotation("Alcsútdoboz", 0.3, (25, -45)) \
-    .add_annotation("Vértesacsa", 0.12, (35, -75)) \
-    .add_annotation("Lovasberény", 0.0001, (-90, 25))
-
-    rgen.add_leg(
-        'Lovasberény->Császár',
-        r'\frac{1}{e^x}',
-        r'$x=\pi$ at Lovasberény, $x=-\frac{\pi}{2}$ at Császár',
-        lambda x: 1. / math.exp(x),
-        [
-            (VFRPoint(18.55314689455907, 47.31145066437747), math.pi),
-            (VFRPoint(18.13993451767051, 47.50100085714328), -math.pi/2)
-        ]
-    ).add_annotation("Lovasberény", math.pi, (-30, -35)) \
-    .add_annotation("Zámolyi vízt", math.pi-1., (-80, -30)) \
-    .add_annotation("Csber/Malmás", math.pi-3., (-100, -40)) \
-    .add_annotation("Mór", math.pi-3.9, (-100, -20)) \
-    .add_annotation("Császár", -math.pi/2, (-90, -40)) \
-
-    rgen.add_leg(
-        'Császár->LHFH',
-        r'\sin\left(x\right)',
-        r'$x=0$ at Császár, $x=\pi$ at LHFH, $x=\frac{\pi}{2}$ at Nyergesújfalu',
-        lambda x: math.sin(x), # pylint disable=unneccessary-lambda
-        [
-            (VFRPoint(18.13993451767051, 47.50100085714328), 0),
-            (VFRPoint(18.54614123145077, 47.7612944935143), math.pi/2),
-            (VFRPoint(18.912424867046774, 47.48950030173632), math.pi)
-        ]
-    ).add_annotation("Császár", 0, (-80, 40)) \
-    .add_annotation("Szákszend", 0.15, (10, 0)) \
-    .add_annotation("M1", 0.5, (-110, 20)) \
-    .add_annotation("Tata", 0.7, (-60, 40)) \
-    .add_annotation("Dsztmikl", 0.93, (0, -80)) \
-    .add_annotation("Lábatlan", 1.35, (-40, 30)) \
-    .add_annotation("Nyergesújfalu", 1.6, (20, 30)) \
-    .add_annotation("Csolnok", 2.25, (-110, -20)) \
-    .add_annotation("Tinnye", 2.57, (-25, 60)) \
-    .add_annotation("Telki", 2.9, (-33, 80)) \
-    .add_annotation("END", math.pi, (-140, 60))
-
-    rgen.set_state(VFRRouteState.ANNOTATIONS)
-
-    # TODO: add tracks
-
-    rgen.finalize()
-
-    return rgen    
+    with open(os.path.join(Path(__file__).parent, 'LHFH--Lovasberény--Császár--Nyergesújfalu--LHFH.vfr'), 'rt', encoding='utf8') as f:
+        rgen = VFRFunctionRoute.fromJSON(''.join(f.readlines()),
+                                         global_requests_session,
+                                         workfolder=os.path.join(rootpath, "data"),
+                                         outfolder=os.path.join(rootpath, "output"),
+                                         tracksfolder=os.path.join(rootpath, "data")
+                                        )
+    
+    return rgen
